21b03d2c1bba8b46fce3e2dfe6e60fe8dcf22734
Aditya Ramesh <_@adityaramesh.com>
2015-10-18 00:08:24 -0400

    Removed old sources.

--- source/drivers/scale_mnist.lua
+++
@@ -1,8 +0,0 @@
-require "torch"
-
-input_fp = arg[1]
-output_fp = arg[2]
-
-data = torch.load(input_fp)
-data.inputs = data.inputs:float():div(255):mul(2):add(-1)
-torch.save(output_fp, data)
--- source/task/svhn_small.lua
+++
@@ -1,8 +0,0 @@
-require "torch"
-
-function get_task_info()
-        local pp_data_dir = "data/svhn/image_normalized"
-        local train_file = paths.concat(pp_data_dir, "train_small_yuv.t7")
-        local test_file = paths.concat(pp_data_dir, "test_yuv.t7")
-        return torch.load(train_file), torch.load(test_file), 10
-end
